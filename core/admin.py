1cc7096956e582556c3bebc035324c221e7e5c8b
Abraao Brandao <abraao.brandao@fpf.br>
2025-10-17 20:24:29 -0400

    Adds user, book, and library admin interfaces
    Registers User, Book, and Library models in Django admin
    interface.
    
    Configures custom UserAdmin to display and filter
    user-related fields.
    
    Configures BookAdmin to display, search, and filter
    book-related fields.
    
    Configures LibraryAdmin to display and search library
    owner and provides a user-friendly interface for
    managing books.
    
    Adds .sqlite3 to .gitignore to exclude the database
    file from version control.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Book, Library
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_librarian')
+    list_filter = ('is_staff', 'is_librarian', 'is_superuser')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'published_year', 'available')
+    search_fields = ('title', 'author')
+    list_filter = ('available', 'published_year')
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner')
+    search_fields = ('owner__username',)
+    filter_horizontal = ('books',)  # para selecionar múltiplos livros facilmente
